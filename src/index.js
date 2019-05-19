21f45935c014d115f3c9eaf4ad9c493cc8b3ba8c
Sam Hunt <samhunt.dev@gmail.com>
2019-05-19 21:00:14 +1200

    feat: Bold the currently selected item in the move list.

--- src/index.js
+++ src/index.js
@@ -92,7 +92,10 @@ class Game extends React.Component {
         'Go to game start';
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button
+            onClick={() => this.jumpTo(move)}
+            className={move === this.state.stepNumber ? 'selected' : 'unselected'}
+          >{desc}</button>
         </li>
       );
     });
